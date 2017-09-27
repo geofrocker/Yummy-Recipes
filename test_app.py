228a433f8776d64832a1983b23f79bcb356f56c7
geofrocker <geofrocker2@gmail.com>
2017-09-27 19:57:00 +0300

    Refactor/update test_app.py include more tests

--- test_app.py
+++ test_app.py
@@ -1,3 +1,26 @@
+"""Test for yummy recipe web app"""
+from app import app
+
+def test_home_page_header():
+    client = app.test_client()
+    rsp = client.get('/')
+    assert rsp.status == '200 OK'
+
+def test_login_page_header():
+    client = app.test_client()
+    rsp = client.get('/login')
+    assert rsp.status == '200 OK'
+
+def test_register_page_header():
+    client = app.test_client()
+    rsp = client.get('/register')
+    assert rsp.status == '200 OK'
+
+def test_dashboard_page_header():
+    client = app.test_client()
+    rsp = client.get('/dashboard')
+    assert rsp.status == '200 OK'
+    
 def test_add_recipe_page_header():
     client = app.test_client()
     rsp = client.post('/add_recipe')
